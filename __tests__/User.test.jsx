52b8f2e3f0a44e7ebaefd0144072777d68afc68e
Mike  Riddelsdell <miker@theodo.co.uk>
2020-11-27 18:03:38 +0000

    test useRouter

---
+++ __tests__/User.test.jsx
@@ -0,0 +1,34 @@
+import User from "../pages/user/[id]";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { RouterContext } from "next/dist/next-server/lib/router-context";
+
+const id = "testid";
+
+export const routerMock = {
+  basePath: "",
+  pathname: "/",
+  route: "/",
+  asPath: "/",
+  query: { id },
+  push: jest.fn().mockResolvedValue(true),
+  replace: jest.fn().mockResolvedValue(true),
+  reload: jest.fn(),
+  back: jest.fn(),
+  prefetch: jest.fn().mockResolvedValue(undefined),
+  beforePopState: jest.fn(),
+  events: {
+    on: jest.fn(),
+    off: jest.fn(),
+    emit: jest.fn(),
+  },
+  isFallback: false,
+};
+
+test("AAU, I can see the user id", () => {
+  render(
+    <RouterContext.Provider value={routerMock}>
+      <User />
+    </RouterContext.Provider>
+  );
+  screen.getByText(id);
+});
--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,7 +1,12 @@
+import { useRouter } from "next/router";
+
 const User = () => {
+  const { query } = useRouter();
+  const { id } = query;
   return (
     <div>
       <h1>User Page</h1>
+      <div>{id}</div>
     </div>
   );
 };
